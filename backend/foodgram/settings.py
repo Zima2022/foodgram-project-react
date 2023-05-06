975b3f6151ad5a9dc40702c698a8771cd61fc2a6
Василий Ямутин <yamutin2010@yandex.ru>
2023-05-07 01:25:43 +0300

    Настроена админка

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--#nih&u0_cjpyppsj=7y#11#gvf1@1!!b0pi(1sr%9h)m&*d@0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.User'
 
@@ -113,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
