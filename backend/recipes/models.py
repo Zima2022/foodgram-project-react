04a81c12a1169ac2c2431c44759801c463935231
Василий Ямутин <yamutin2010@yandex.ru>
2023-05-08 20:52:17 +0300

    Добавлены модели Recipe, RecipeIngredient, Favorite, ShoppingCart

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,4 +1,8 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from django.db.models import UniqueConstraint
+
+from users.models import User
 
 
 class Tag(models.Model):
@@ -46,3 +50,136 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return f'{self.name}, {self.measurement_unit}'
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=200
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to='recipes/'
+    )
+    text = models.TextField(
+        verbose_name='Описание'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        verbose_name='Ингридиенты',
+        through='RecipeIngredient',
+        related_name='recipes',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        related_name='recipes'
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        help_text='Введите время приготовления в минутах',
+        validators=[MinValueValidator(
+            1,
+            message='Время приготовления не может быть меньше минуты')
+        ],
+        verbose_name='Время приготовления мин.',
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ('-id',)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE,
+        related_name='recipeingredients'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        verbose_name='Ингредиент',
+        on_delete=models.PROTECT
+    )
+    amount = models.PositiveSmallIntegerField(
+        verbose_name='Количество ингредиента',
+        validators=[MinValueValidator(
+            1,
+            message='Количество должно быть больше одного')
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент рецепта'
+        verbose_name_plural = 'Ингредиенты рецепта'
+        ordering = ('-id',)
+        constraints = [
+            UniqueConstraint(
+                fields=('recipe', 'ingredient'),
+                name='unique_recipe_ingredient',
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.recipe.name} - {self.ingredient.name}'
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='favorites'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE,
+        related_name='favorites'
+    )
+
+    class Meta:
+        verbose_name = 'Избранное'
+        verbose_name_plural = 'Избранное'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique_favorite'
+            ),
+        )
+
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        related_name='shoppingcarts'
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        verbose_name='Рецепт',
+        on_delete=models.CASCADE,
+        related_name='shoppingcarts'
+    )
+
+    class Meta:
+        verbose_name = 'Список покупок'
+        verbose_name_plural = 'Списки покупок'
+        ordering = ('user',)
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'recipe'),
+                name='unique_shoppingcart'
+            ),
+        )
