815df32283bc64de2c20bb86f68342b3e58bc63e
Василий Ямутин <yamutin2010@yandex.ru>
2023-05-10 01:59:19 +0300

    Отредактирована админка

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
+from django.contrib.admin import display
 
-from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
+from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
+                            ShoppingCart, Tag)
+from users.models import Follow
 
 
 class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
-    extra = 1
+    extra = 0
 
 
 @admin.register(Tag)
@@ -25,6 +28,35 @@ class IngredientAdmin(admin.ModelAdmin):
 class RecipeAdmin(admin.ModelAdmin):
     inlines = (RecipeIngredientInline,)
     list_display = (
-        'id', 'name', 'author', 'cooking_time')
+        'id', 'name', 'author', 'cooking_time', 'count_favorited')
     search_fields = ('name', 'author__username', 'author__email',)
+    readonly_fields = ('count_favorited',)
     list_filter = ('tags',)
+
+    @display(description='Количество в избранных')
+    def count_favorited(self, obj):
+        return obj.favorites.count()
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe',)
+    search_fields = ('user', 'recipe',)
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'ingredient',)
+    search_fields = ('recipe',)
+
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'author',)
+    search_fields = ('user', 'author',)
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'recipe',)
+    search_fields = ('user', 'recipe',)
